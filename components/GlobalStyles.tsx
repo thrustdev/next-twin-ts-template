74f302f5f7bae52be9587bf2d40dd1c7092b962e
Adrian Moses <adrian@adrianmoses.com>
2021-04-27 21:34:25 -0400

    initializes template

---
+++ components/GlobalStyles.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+import { GlobalStyles as BaseStyles } from 'twin.macro'
+
+const CustomStyles = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
+
+const GlobalStyles = () => (
+  <>
+    <BaseStyles />
+    <CustomStyles />
+  </>
+)
+
+export default GlobalStyles
